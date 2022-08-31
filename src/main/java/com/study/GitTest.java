4e97ee0015b740abbb3ea588e8327fa96a0a269c
pongo <2359808968@qq.com>
2022-08-31 16:50:20 +0800

    hot-fix first commit

--- src/main/java/com/study/GitTest.java
+++ src/main/java/com/study/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hh");
         System.out.println("hhh");
+        System.out.println("hhh1");
     }
 }
